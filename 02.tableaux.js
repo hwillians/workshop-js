a5ad265afed3e9b83b341e9771ecce1046245704
Helvin <hjwc86@gmail.com>
2020-09-16 12:05:12 +0200

    TP 02 - Les tableaux

--- 02.tableaux.js
+++ 02.tableaux.js
@@ -1,11 +1,28 @@
 var villes = ['nantes', 'paris', 'saint-nazaire', 'angers', 'le mans'];
 
-villes.forEach(function(element){
+villes.forEach(function (element) {
     console.log(element);
 });
 
-console.log(villes.every(function(element){
-    return element.includes('a');
-  }));
+console.log("lettreADansToutesLesVilles = " + villes.every(function (element) {
+    return element.includes("a");
+}));
 
-  
+console.log("auMoinsUneVilleAvecUnTiret = " + villes.some(function (element) {
+    return element.includes('-');
+}));
+
+console.log( "villesSansTiretSansEspace = " + villes.filter(function (element) {
+    return !element.includes(' ') && !element.includes('-');
+}))
+
+
+
+var villesMajusculeSeTerminantParS = villes.filter(function (element) {
+    return element.includes('s', element.length - 1);
+})
+
+console.log("villesMajusculeSeTerminantParS = "+villesMajusculeSeTerminantParS.map(function(element){
+   return element.toUpperCase()
+}))
+
