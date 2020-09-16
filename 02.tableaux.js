5714fc7d069c6e37955c64d0b90b0e9f626134e8
Helvin <hjwc86@gmail.com>
2020-09-16 10:50:15 +0200

    every()

--- 02.tableaux.js
+++ 02.tableaux.js
@@ -3,3 +3,7 @@ var villes = ['nantes', 'paris', 'saint-nazaire', 'angers', 'le mans'];
 villes.forEach(function(element){
     console.log(element);
 });
+
+console.log(villes.every(function(element){
+    return element.includes('a');
+  }));
